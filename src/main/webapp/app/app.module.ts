8d8a75cdc6853680caf8d335f47807453e012348
Renan Franca <renan.andradefranca@gmail.com>
2021-11-01 15:56:01 -0300

    Configure Progressive Web App (PWA)

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,32 +1,31 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import locale from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { TranslateModule, TranslateService, TranslateLoader, MissingTranslationHandler } from '@ngx-translate/core';
-import { NgxWebstorageModule, SessionStorageService } from 'ngx-webstorage';
-import * as dayjs from 'dayjs';
 import { NgbDateAdapter, NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
-
+import { MissingTranslationHandler, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
-import './config/dayjs';
+import { httpInterceptorProviders } from 'app/core/interceptor/index';
 import { SharedModule } from 'app/shared/shared.module';
+import * as dayjs from 'dayjs';
+import { NgxWebstorageModule, SessionStorageService } from 'ngx-webstorage';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeModule } from './home/home.module';
-import { EntityRoutingModule } from './entities/entity-routing.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { NgbDateDayjsAdapter } from './config/datepicker-adapter';
+import './config/dayjs';
 import { fontAwesomeIcons } from './config/font-awesome-icons';
-import { httpInterceptorProviders } from 'app/core/interceptor/index';
-import { translatePartialLoader, missingTranslationHandler } from './config/translation.config';
-import { MainComponent } from './layouts/main/main.component';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
-import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
+import { missingTranslationHandler, translatePartialLoader } from './config/translation.config';
+import { EntityRoutingModule } from './entities/entity-routing.module';
+import { HomeModule } from './home/home.module';
 import { ErrorComponent } from './layouts/error/error.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MainComponent } from './layouts/main/main.component';
+import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 
 @NgModule({
   imports: [
@@ -37,7 +36,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     EntityRoutingModule,
     AppRoutingModule,
     // Set this to true to enable service worker (PWA)
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: false }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true }),
     HttpClientModule,
     NgxWebstorageModule.forRoot({ prefix: 'jhi', separator: '-', caseSensitive: true }),
     TranslateModule.forRoot({
