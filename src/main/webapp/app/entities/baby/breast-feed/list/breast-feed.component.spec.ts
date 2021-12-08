0550e25dbc77c70127710f26e09c339652ddf540
Renan Franca <renan.andradefranca@gmail.com>
2021-12-08 12:58:52 -0300

    Fix breastfeed unit test

--- src/main/webapp/app/entities/baby/breast-feed/list/breast-feed.component.spec.ts
+++ src/main/webapp/app/entities/baby/breast-feed/list/breast-feed.component.spec.ts
@@ -1,10 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
-
 import { BreastFeedService } from '../service/breast-feed.service';
-
 import { BreastFeedComponent } from './breast-feed.component';
 
 describe('Component Tests', () => {
@@ -54,12 +52,12 @@ describe('Component Tests', () => {
       expect(comp.breastFeeds[0]).toEqual(expect.objectContaining({ id: 123 }));
     });
 
-    it('should calculate the sort attribute for an id', () => {
+    it('should calculate the sort attribute for a start', () => {
       // WHEN
       comp.ngOnInit();
 
       // THEN
-      expect(service.query).toHaveBeenCalledWith(expect.objectContaining({ sort: ['id,asc'] }));
+      expect(service.query).toHaveBeenCalledWith(expect.objectContaining({ sort: ['start,desc'] }));
     });
 
     it('should calculate the sort attribute for a non-id attribute', () => {
@@ -73,7 +71,7 @@ describe('Component Tests', () => {
       comp.loadPage(1);
 
       // THEN
-      expect(service.query).toHaveBeenLastCalledWith(expect.objectContaining({ sort: ['name,asc', 'id'] }));
+      expect(service.query).toHaveBeenLastCalledWith(expect.objectContaining({ sort: ['name,desc', 'start'] }));
     });
 
     it('should re-initialize the page', () => {
